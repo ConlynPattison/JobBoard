106cfce9c1309a00d07aa03ce152d54ef8e1fb97
ConlynPattison <cpattison@csumb.edu>
2023-11-14 01:15:19 -0800

    Fixed Backend nightmare

--- backend/src/main/java/com/cst438/domain/UserRepository.java
+++ backend/src/main/java/com/cst438/domain/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository <User, Integer> {
 
     @Query("select a from User a where a.username= :username")
