d5bedca9a87240725a12ee8389fb8bf9c85b5bc9
ConlynPattison <cpattison@csumb.edu>
2023-12-03 22:15:17 -0800

    Added testing data and backend skeleton

--- backend/src/main/java/com/cst438/controllers/SavedListingsController.java
+++ backend/src/main/java/com/cst438/controllers/SavedListingsController.java
@@ -1,7 +1,7 @@
 package com.cst438.controllers;
 
-import com.cst438.domain.SavedListingDTO;
-import com.cst438.domain.SavedListingState;
+import com.cst438.domain.*;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,33 +11,69 @@ import java.security.Principal;
 @RestController
 public class SavedListingsController {
 
+    private final ListingRepository listingRepository;
+    private final SavedListingRepository savedListingRepository;
+
+    public SavedListingsController(ListingRepository listingRepository,
+                                   SavedListingRepository savedListingRepository) {
+        this.listingRepository = listingRepository;
+        this.savedListingRepository = savedListingRepository;
+    }
+
     @RequestMapping(value = "/api/saved/{id}", method = RequestMethod.GET)
     public SavedListingDTO getSavedListing(Principal principal) {
-        // TODO:
+        // authorize the user request
+
+        // get the saved_listing entry with this saved_listing id
+
+        // validate the user_id is the same as the requesting users' -> return error if not
+
+        // cast the saved_listing to a DTO -> return this DTO
+
         return new SavedListingDTO(0, SavedListingState.SAVED, 0, 0);
     }
 
     @RequestMapping(value = "/api/saved", method = RequestMethod.GET)
     public SavedListingDTO[] getAllSavedListings(Principal principal) {
-        // TODO:
+        // authorize the user request
+
+        // get all saved_listing entries with this user_id
+
+        // cast these saved_listing entries to DTO[] -> return this array
+
         return new SavedListingDTO[]{
                 new SavedListingDTO(0, SavedListingState.SAVED, 0, 0)
         };
     }
 
     @RequestMapping(value = "/api/saved", method = RequestMethod.POST)
-    public int createSavedListing(Principal principal) {
-        // TODO:
+    public int createSavedListing(Principal principal,
+                                  @RequestBody SavedListingDTO savedListingDTO,
+                                  @RequestBody ListingDTO listingDTO) {
+        // authorize the user request
+
+        // check if the listing exists in the db already -> create it if not
+
+        // check if the user has already saved this listing -> exit request if so
+
+        // create the saved_listing record -> return the id
+
         return 1;
     }
 
     @RequestMapping(value = "/api/saved/{id}", method = RequestMethod.PATCH)
     public void updateSavedListing(Principal principal) {
-        // TODO:
+        // authorize the user request
+
+        // check if the listing exists -> if not exit with error
+
+        // change the value of the saved_listing state to that provided
     }
 
     @RequestMapping(value = "/api/saved/{id}", method = RequestMethod.DELETE)
     public void removeSavedListing(Principal principal) {
-        // TODO:
+        // authorize the user request
+
+        // delete the saved_listing
     }
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,47 +1,17 @@
-
---INSERT INTO course (year, semester, course_id, title, instructor)  VALUES
---(2020,'Fall',30157,'BUS 203 - Financial Accounting','cchou@csumb.edu'),
---(2020,'Fall',30163,'BUS 306 - Fundamentals of Marketing','anariswari@csumb.edu'),
---(2020,'Fall',30291,'BUS 304 - Business Communication, Pro-seminar & Critical Thinking','kposteher@csumb.edu'),
---(2020,'Fall',31045,'CST 363 - Introduction to Database Systems','dwisneski@csumb.edu'),
---(2020,'Fall',31249,'CST 237 - Intro to Computer Architecture','sislam@csumb.edu'),
---(2020,'Fall',31253,'BUS 307 - Finance','hwieland@csumb.edu'),
---(2020,'Fall',31747,'CST 238 - Introduction to Data Structures','jgross@csumb.edu'),
---(2020,'Fall',40443,'TEST - Test Course', 'dwisneski@csumb.edu')
---;
---
---insert into assignment (id, due_date, name, course_id) values
---(1, '2021-09-01', 'db design', 31045),
---(2, '2021-09-02', 'requirements', 31045),
---(3, '2021-10-01', 'testing design', 40443),
---(4, '2021-10-01', 'testing design part2', 40443)
---;
---
---insert into enrollment (id, student_email, student_name, course_id)  values
---(1, 'test@csumb.edu', 'test', 31045),
---(2, 'dwisneski@csumb.edu', 'david', 31045),
---(3, 'trebold@csumb.edu', 'tom', 31045),
---(4, 'test4@csumb.edu', 'test4', 31045),
---(5, 'trebold@csumb.edu', 'tom', 40443),
---(6, 'trebold@fresno.edu', 'bob',40443)
---;
---
---insert into assignment_grade (score, assignment_id, enrollment_id) values
---(90, 1, 1),
---(91, 1, 2),
---(92, 1, 3),
---(93, 2, 1),
---(94, 2, 2),
---(95, 2, 3),
---(91, 3, 5),
---(92, 4, 5),
---(93, 3, 6),
---(94, 4, 6)
---;
-
 insert into user_table
 (username, email, password, role) values
 ('test', 'dwisneski@csumb.edu', '$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','USER'),
 ('admin', 'admin@csumb.edu', '$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW' , 'ADMIN')
 ;
 
+insert into listing
+(id, job_title, company_logo_url, application_url) values
+(1, 'google engineer', 'google.com', 'youtube.com'),
+(2, 'facebook engineer', 'facebook.com', 'twitter.com')
+;
+
+insert into saved_listing
+(id, listing_id, user_id, state) values
+(1, 2, 1, 'applied'),
+(2, 1, 1, 'assessment')
+;
