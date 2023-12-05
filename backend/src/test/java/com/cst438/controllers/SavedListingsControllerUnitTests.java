ea44d1afe300aa71bca3fedeb7d46b913c407317
ConlynPattison <cpattison@csumb.edu>
2023-12-04 21:51:00 -0800

    Created test functions for SavedListingsController

--- backend/src/test/java/com/cst438/controllers/SavedListingsControllerUnitTests.java
+++ backend/src/test/java/com/cst438/controllers/SavedListingsControllerUnitTests.java
@@ -1,9 +1,50 @@
 package com.cst438.controllers;
 
+import com.cst438.domain.ListingRepository;
+import com.cst438.domain.SavedListingRepository;
+import com.cst438.services.JwtService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SavedListingsControllerUnitTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private SavedListingRepository savedListingRepository;
+
+    @Autowired
+    private ListingRepository listingRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    private static MockHttpServletResponse response;
+
+    @Test
+    public void getSavedListing() throws Exception {
+        // todo:
+    }
+
+    @Test
+    public void createSavedListing() throws Exception {
+
+    }
+
+    @Test
+    public void updateSavedListing() throws Exception {
+
+    }
+
+    @Test
+    public void removeSavedListingExternal() throws Exception {
+
+    }
 }
--- frontend/src/components/JobResults/JobCard.jsx
+++ frontend/src/components/JobResults/JobCard.jsx
@@ -11,7 +11,6 @@ import {
 	Image,
 } from "@chakra-ui/react";
 import React from "react";
-import axios from "axios";
 import { useAuth } from "../../hooks/useAuth.ts";
 
 const JobCard = ({ jobData, isSaved, setSavedListing }) => {
