2fd02ffc436ad587fed7cc631fa7e5307ae5c359
MGarc758 <83996652+MGarc758@users.noreply.github.com>
2023-12-04 23:22:45 -0800

    Added mapping for SavedListings on Profile page

--- frontend/src/routes/ProfilePage.jsx
+++ frontend/src/routes/ProfilePage.jsx
@@ -1,12 +1,37 @@
 import NavHeaders from "../components/NavHeader";
-import React from "react";
-import {Card, CardBody, Image, Text} from "@chakra-ui/react";
+import React, {useState} from "react";
+import {Button, Card, CardBody, Image, Table, Text} from "@chakra-ui/react";
 import {Center} from "@chakra-ui/layout";
-import {useAuth} from "../hooks/useAuth";
+import {useAuth} from "../hooks/useAuth.ts";
+import axios from "axios";
 
 
 const ProfilePage = () => {
     const { user, logout } = useAuth();
+    const [savedListings, setSavedListings] = useState([]);
+
+    const removeListing = (externalId) => {
+        axios.delete("api/saved/external/" + externalId, {
+            headers: {
+                Authorization: user?.token || ""
+            }
+        }).then((response) => {
+            setSavedListings(prev => [...prev].filter((listing) => listing.externalId !== externalId))
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+    const fetchSavedListings = async() => {
+        axios.get("/api/saved/details", {
+            headers: {
+                Authorization: user?.token || ""
+            }
+        }).then(({ data }) => {
+            setSavedListings(data);
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
 
     return (
         <>
@@ -25,7 +50,39 @@ const ProfilePage = () => {
             <br />
             <Card>
                 <CardBody>
-                    Text
+                    <Table>
+                        {savedListings.map((listingDetails) => (
+                            <tr>
+                                <td>
+                                    {listingDetails.jobTitle}
+                                </td>
+                                <td>
+                                    <Image boxSize='30px' src={listingDetails.companyLogoUrl} alt='Company Logo'/>
+                                </td>
+                                <td>
+                                    <Button
+                                        variant="solid"
+                                        colorScheme="blue"
+                                        onClick={() => {
+                                            window.open(listingDetails.applicationUrl);
+                                        }}
+                                    >
+                                        Apply
+                                    </Button>
+                                </td>
+                                <td>
+                                    <Button
+                                        variant="solid"
+                                        colorScheme="red"
+                                        onClick={() => removeListing(listingDetails.externalId)}
+                                    >
+                                        Remove
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+
+                    </Table>
                 </CardBody>
             </Card>
         </>
